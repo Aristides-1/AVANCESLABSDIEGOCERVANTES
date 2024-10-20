9a24575d716d7f8cbfa702d6c2e8c14b0890b4a7
Aristides-1 <dcervantesapa@unsa.edu.pe>
2024-10-19 23:41:31 -0500

    Add files via upload

--- ProblemasExtraPERL/correo.pl
+++ ProblemasExtraPERL/correo.pl
@@ -6,15 +6,15 @@ print "Ingrese su nombre de usuario:\n";
 
 #Ingreso de usuario
 my $user=<STDIN>;
-
+chomp($user);
 #Petición de ingreso de dominio 
 print "Ingrese el dominio de su correo:\n";
 
 #Ingreso de dominio
 my $dominio=<STDIN>;
+chomp($dominio);
 
-print "Su correo electrónico es:\n";
-print "$user";
-print "@";
-print "$dominio";
+#Impresión de correo electrónico
+print "Su correo electrónico es:";
+print "$user\@$dominio";
 
