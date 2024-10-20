86959b6efa9f18bf4ae338c235ecb01347b8e968
Aristides-1 <dcervantesapa@unsa.edu.pe>
2024-10-19 22:58:24 -0500

    Add files via upload

---
+++ ProblemasExtraPERL/correo.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+#Petición de ingreso de usuario
+print "Ingrese su nombre de usuario:\n";
+
+#Ingreso de usuario
+my $user=<STDIN>;
+
+#Petición de ingreso de dominio 
+print "Ingrese el dominio de su correo:\n";
+
+#Ingreso de dominio
+my $dominio=<STDIN>;
+
+
